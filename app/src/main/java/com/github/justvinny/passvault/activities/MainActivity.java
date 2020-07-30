4b02ab6538fde8d20904a30a6768b3f3441cd36c
Vinson Beduya <vinsonemb.151994@gmail.com>
2020-07-30 17:28:45 +1200

    Login functionality completed. Automatically sets password entered on first run as password using SharedPreferences.

--- app/src/main/java/com/github/justvinny/passvault/activities/MainActivity.java
+++ app/src/main/java/com/github/justvinny/passvault/activities/MainActivity.java
@@ -2,45 +2,100 @@ package com.github.justvinny.passvault.activities;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.justvinny.passvault.R;
-import com.github.justvinny.passvault.files.ReadText;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int MENU_RESULT = 0;
+    public static final String SHARED_PREFS = "SHARED_PREFS";
     private static final String TAG = "MainActivity";
 
+    private ConstraintLayout mainLayout;
+    private TextView textViewFirstRun;
     private EditText editTextPassword;
     private Button buttonLogin;
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor sharedPreferencesEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textViewFirstRun = (TextView) findViewById(R.id.textViewFirstRunMsg);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
-
         buttonLogin = (Button) findViewById(R.id.button_login);
+        sharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        sharedPreferencesEditor = sharedPreferences.edit();
+
+        if (checkIfFirstRun()) {
+            firstRunMessage();
+        }
+
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String passwordEntered = editTextPassword.getText().toString();
+                String password = editTextPassword.getText().toString();
 
-                if (ReadText.readText(getApplicationContext(), passwordEntered)) {
-                    launchMainMenu();
+                if (checkIfFirstRun()) {
+                    if (!password.isEmpty()) {
+                        passwordSuccessfullyStored(password);
+                    } else {
+                        passwordEmptyMessage();
+                    }
+                }
+
+                if (!password.isEmpty()) {
+                    if (passwordEnteredMatched()) {
+                        launchMainMenu();
+                    } else {
+                        passwordIncorrectMessage();
+                    }
                 } else {
-                    loginFailedMessage();
+                    passwordEmptyMessage();
                 }
             }
         });
+    }
+
+    private boolean checkIfFirstRun() {
+        return sharedPreferences.getBoolean("first_run", true);
+    }
+
+    private void firstRunMessage() {
+        String message = "First login detected.\nEnter password to be stored and press login.";
+        textViewFirstRun.setText(message);
+        textViewFirstRun.setVisibility(View.VISIBLE);
+    }
+
+    private void passwordSuccessfullyStored(String password) {
+        sharedPreferencesEditor.putBoolean("first_run", false);
+        sharedPreferencesEditor.putString("password", password);
+        sharedPreferencesEditor.apply();
+    }
+
+    private boolean passwordEnteredMatched() {
+        String passwordEntered = editTextPassword.getText().toString();
+        String passwordStored = sharedPreferences.getString("password", "");
+
+        return passwordEntered.equals(passwordStored);
     }
 
     private void launchMainMenu() {
@@ -48,8 +103,13 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(loginIntent, MENU_RESULT);
     }
 
-    private void loginFailedMessage() {
+    private void passwordIncorrectMessage() {
         Toast.makeText(getApplicationContext(), "Wrong password", Toast.LENGTH_LONG).show();
+    }
+
+    private void passwordEmptyMessage() {
+        Toast.makeText(getApplicationContext(), "Password can't be empty!", Toast.LENGTH_LONG)
+                .show();
     }
 
     @Override
